df22c44e800d6797eb3c977162ba8a19cdd01cc0
Mitia <raharilala.fifaliana@gmail.com>
2026-03-06 22:51:21 +0300

    second uptload

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('output.css') }}">
     @yield('tomselect')
-    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     <script src="{{ asset('output.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/intl-tel-input@17.0.19/build/css/intlTelInput.css">
