06555d2c5e9d97ea87af16e23a4e9a210f86bf57
Mitia <raharilala.fifaliana@gmail.com>
2026-05-02 22:31:45 +0300

    Updating factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,14 +18,12 @@ class UserFactory extends Factory
 
     /**
      * Define the model's default state.
-     *
-     * @return array<string, mixed>
      */
     public function definition(): array
     {
         return [
-            'name' => $this->fake()->name(),
-            'email' => $this->fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
